8301d7992e65f77881fc58dfcada53a4b1b8c517
s3759957 <s3759957@rmit.edu.vn>
2022-07-21 13:08:57 +0700

    Initial Commit

---
+++ asm1_final/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  asm1_final
+//
+//  Created by Nguyen, Nguyen Dinh Dang on 21/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ asm1_final/asm1_finalApp.swift
@@ -0,0 +1,17 @@
+//
+//  asm1_finalApp.swift
+//  asm1_final
+//
+//  Created by Nguyen, Nguyen Dinh Dang on 21/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct asm1_finalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
